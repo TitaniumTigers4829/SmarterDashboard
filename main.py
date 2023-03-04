e390445846cb49f23827a355726834c7affe2249
Ari Davidson <ardavidson@students.chccs.k12.nc.us>
2023-03-03 23:26:05 -0500

    created very base of smarter dashboard

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-import sys
-
-from PyQt6.QtWidgets import QApplication
-
-from main_window import MainWindow
+import dearpygui.dearpygui as dpg
 
 
-def main():
-    app = QApplication(sys.argv)
-
-    window = MainWindow()
-    window.show()
-
-    app.exec()
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    main()
+    dpg.create_context()
+    dpg.create_viewport(title='Smarter Dashboard', width=500, height=500)
+
+
+    with dpg.window(tag='Smarter Dashboard'):
+        with dpg.drawlist(width=100.0, height=100.0, tag='drawlist', parent='Smarter Dashboard'):
+            dpg.draw_rectangle(pmin=[0.00, 0.0], pmax=[100.0, 100.0])
+        dpg.add_text('Display stuff here')
+
+
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.set_primary_window('Smarter Dashboard', True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
