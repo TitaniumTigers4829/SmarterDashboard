9fbd4b2fcc391b22de011dd718e3a0be03a74bc8
Ari Davidson <ardavidson@students.chccs.k12.nc.us>
2023-03-03 23:27:46 -0500

    created basic stuff

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import dearpygui.dearpygui as dpg
 
 
 
-if __name__ == '__main__':
-    dpg.create_context()
-    dpg.create_viewport(title='Smarter Dashboard', width=500, height=500)
+dpg.create_context()
+dpg.create_viewport(title='Smarter Dashboard', width=500, height=500)
 
 
-    with dpg.window(tag='Smarter Dashboard'):
-        with dpg.drawlist(width=100.0, height=100.0, tag='drawlist', parent='Smarter Dashboard'):
-            dpg.draw_rectangle(pmin=[0.00, 0.0], pmax=[100.0, 100.0])
-        dpg.add_text('Display stuff here')
+with dpg.window(tag='Smarter Dashboard'):
+    with dpg.drawlist(width=100.0, height=100.0, tag='drawlist', parent='Smarter Dashboard'):
+        dpg.draw_rectangle(pmin=[0.00, 0.0], pmax=[100.0, 100.0])
+    dpg.add_text('Display stuff here')
 
 
 
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
-    dpg.set_primary_window('Smarter Dashboard', True)
-    dpg.start_dearpygui()
-    dpg.destroy_context()
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window('Smarter Dashboard', True)
+dpg.start_dearpygui()
+dpg.destroy_context()
